fa1a0a6cc8e0d9e34b80b1a73e6beb7920a393a0
Admin <admin@MacBook.local>
2019-01-21 20:15:17 +0000

    Replaces bikes with doubles in docking station tests

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,52 +1,50 @@
 require 'docking_station'
 
 describe DockingStation do 	
+	
+	let(:working_bike) {double(:bike, :working? => true )}
+	let(:broken_bike) {double(:bike, :working? => false )}
+
 	it { is_expected.to respond_to :release_bike }
 	it { is_expected.to respond_to(:dock).with(1).argument }
 	it { is_expected.to respond_to :bikes }
 
 	it 'has a default capacity of 20 bikes' do 
-		20.times { subject.dock(Bike.new)}
-		expect{(subject.dock(Bike.new))}.to raise_error 'No Space Available'
+		20.times { subject.dock(working_bike)}
+		expect{(subject.dock(working_bike))}.to raise_error 'No Space Available'
 	end
 
 	it 'can be made with larger capacity' do
 		station = DockingStation.new(25)
-		25.times { station.dock(Bike.new)}
-		expect{(station.dock(Bike.new))}.to raise_error 'No Space Available'
+		25.times { station.dock(working_bike)}
+		expect{(station.dock(working_bike))}.to raise_error 'No Space Available'
 	end	
 
 	describe '#dock(bike)' do 
 		it 'can dock bikes' do 
-			bike = Bike.new
-			expect(subject.dock(bike)).to eq bike
+			expect(subject.dock(working_bike)).to eq working_bike
 		end
 		it 'returns docked bikes' do
-			bike = Bike.new
-			subject.dock(bike)
-			expect(subject.release_bike).to eq bike
+			subject.dock(working_bike)
+			expect(subject.release_bike).to eq working_bike
 		end
 		it 'raises an error if full' do 
-			20.times {subject.dock(Bike.new)}
-			expect { subject.dock(Bike.new)}.to raise_error 'No Space Available'
+			20.times {subject.dock(working_bike)}
+			expect { subject.dock(working_bike)}.to raise_error 'No Space Available'
 		end
 	end
 
 	describe '#release_bike' do 
 		it 'releases a bike' do 
-			bike = Bike.new
-			subject.dock(bike)
-			expect(subject.release_bike).to eq bike
+			subject.dock(working_bike)
+			expect(subject.release_bike).to eq working_bike
 		end
 		it 'releases working bikes' do 
-			subject.dock(Bike.new)
-			bike = subject.release_bike
-			expect(bike).to be_working
+			subject.dock(working_bike)
+			expect(subject.release_bike).to be_working
 		end
 
 		it "doesn't release broken bikes" do 
-			bike = Bike.new
-			broken_bike = bike.report_broken
 			subject.dock(broken_bike)
 			expect{(subject.release_bike)}.to raise_error 'No Working Bikes Available'
 		end
